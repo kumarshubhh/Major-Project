82762c75f23aa5bd7657fba6daeee9ad3713a0fd
kumarshubhh <subhanshukumar290@gmail.com>
2024-12-12 00:36:01 +0530

    deploy updated

--- app.js
+++ app.js
@@ -150,10 +150,20 @@ app.use((err, req, res, next)=>{
     res.status(statusCode).render("error.ejs", {err})
    // res.status(statusCode).send(message);
 })
-app.all("*", (req, res, next)=>{
-    next(new EpressErr(404, "page not Found"))
-})
+// app.all("*", (req, res, next)=>{
+//     next(new EpressErr(404, "page not Found"))
+// })
 
+// Catch-all route for undefined endpoints
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Page not found' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong' });
+});
 
 
 
